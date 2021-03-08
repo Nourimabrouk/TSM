9351eb893e68418bfbf5f8274facd161c7de0304
Nouri <46678756+NM@users.noreply.github.com>
2021-03-09 00:31:00 +0100

    Create structure.R

---
+++ structure.R
@@ -0,0 +1,37 @@
+#Structure:
+  #Functions:
+    # optimize_parameters
+    # print_optimizer_output
+    # compute_loglikelihood
+    # compute_kalmanfilter
+    # perform_QML_routine
+    # compute_smoothed_state
+    # perform_particlefilter_routine
+    # draw_values
+    # compute_normalised_weights
+    # compute_att_ptt
+    # resampling
+# a
+  # Transform data
+  # Present Descriptives
+  # Plot Graphs
+# b
+  # Transform
+  # Present graph
+# c
+  # QML
+  # Kalman filter
+# d
+  # Kalman filter
+  # Kalman smoother
+  # Present Ht
+# e
+  # Transform RV
+  # Include Beta & Estimate
+# f
+  # Particle filter
+  # perform_particlefilter_routine
+    # draw_values
+    # compute_normalised_weights
+    # compute_att_ptt
+    # resampling
